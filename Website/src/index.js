65e42658f86fc1997dfec9e75ececd54aca7dd47
kk <48865394+zenithos-k@users.noreply.github.com>
2026-08-09 00:02:46 +0800

    Simplify landing page feature cards

--- Website/src/index.js
+++ Website/src/index.js
@@ -1,9 +1,11 @@
+const currentRelease = "1.4.3.1";
+
 export default {
   fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/" && !url.searchParams.has("release")) {
-      url.searchParams.set("release", "1.4.3");
+    if (url.pathname === "/" && url.searchParams.get("release") !== currentRelease) {
+      url.searchParams.set("release", currentRelease);
 
       return new Response(null, {
         status: 302,
